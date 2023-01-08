2fcbe99bf65fbdbb3fb6be5ce7bbb6fb10761732
Azhar Siddiqi <azhar@siddiquie.net>
2023-01-08 06:30:03 +0200

    bug Fix

--- AdminUstad/pages/Plugin/Brands/Pages/Add.php
+++ AdminUstad/pages/Plugin/Brands/Pages/Add.php
@@ -5,13 +5,33 @@ if(isset($_POST["SubmitForm"])){
     $sql = "SELECT Id FROM `sh_pl_brand_names` WHERE `Name`='$ValueBrandName'";
 	$result = $con->query($sql);
 	if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $ExistBrandNameID=$row["Id"];
         $Error_Name="This Brand name is alrady exist!";
     } else {
-        $sql = "INSERT INTO `sh_pl_brand_names` (`Status`, `Name`)
-		VALUES ('$ValueBrandStatus', '$ValueBrandName')";
-		if($con->query($sql)===TRUE){
-			header("Location: /".$AdminFolder."/Plugin/".$Plugin."/dashboard");
-		}
+		$sql = "INSERT INTO `sh_pl_brand_names` (`Status`, `Name`)
+        VALUES ('$ValueBrandStatus', '$ValueBrandName')";
+    }
+
+    if(isset($_POST["EditBarndNameID"]) && $_POST["EditBarndNameID"]==$ExistBrandNameID){
+        $sql = "UPDATE `sh_pl_brand_names` SET `Status`='$ValueBrandStatus', `Name`='$ValueBrandName' WHERE `Id`='$_POST[EditBarndNameID]'";
+    } else {
+        
+    }
+    if($con->query($sql)===TRUE){
+        header("Location: /".$AdminFolder."/Plugin/".$Plugin."/dashboard");
+    }
+}
+if(isset($_GET["EditBarndNameID"])){
+    $sql = "SELECT * FROM `sh_pl_brand_names` WHERE `Id`='$_GET[EditBarndNameID]'";
+	$result = $con->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if(!isset($_POST["SubmitForm"])){
+            $ValueBrandStatus=$row["Status"];
+            $ValueBrandName=$row["Name"];
+        }
+        
     }
 }
 ?><!DOCTYPE html>
@@ -38,16 +58,18 @@ if(isset($_POST["SubmitForm"])){
 		<form action='' Method='POST' class=" pt-3">
             <div class="form-floating">
   				<select class="form-select" id="sel1" name="FormBrand_Status">
-   					<option>Enable</option>
-   					<option>Disable</option>
+   					<option <?php if(isset($ValueBrandStatus) && $ValueBrandStatus=="Enable"){ echo "selected"; } ?>>Enable</option>
+   					<option <?php if(isset($ValueBrandStatus) && $ValueBrandStatus=="Disable"){ echo "selected"; } ?>>Disable</option>
   				</select>
   				<label for="sel1" class="form-abel">Select list (select one):</label>
 			</div>
             <div class="form-floating mb-3 mt-3">
-      			<input name="FormBrand_Name" class="form-control"  placeholder="Link" >
+      			<input name="FormBrand_Name" class="form-control"  placeholder="Brand Name" value="<?php if(isset($ValueBrandName)){ echo stripslashes($ValueBrandName); } ?>" >
       			<label for="Link">Brand name</label>
    			</div>
-
+            <?php if(isset($_GET["EditBarndNameID"])){ ?>
+                <input type="hidden" name="EditBarndNameID" value="<?php echo $_GET["EditBarndNameID"]; ?>">
+            <?php } ?>
             <button type="submit" name="SubmitForm" value="BrandName" class="btn btn-lg  btn-primary w-100">Submit</button>
 
         </form>
--- AdminUstad/pages/Plugin/Brands/dashboard.php
+++ AdminUstad/pages/Plugin/Brands/dashboard.php
@@ -12,7 +12,7 @@ if(isset($_GET["DeleteBarndNameID"])){
 	<?php include("pages/inc/Header.php"); ?>
 </head>
 <body class="v3">
-<?php \template\navbar(array("Btn_Search"=>false,"Btn_BackLink"=>"$AdminFolder/Plugin/$Plugin/dashboard")); ?>
+<?php \template\navbar(array("Btn_Search"=>false)); ?>
 <?php if(is_file("pages/Plugin/".$Plugin."/Sidebar.php")){ include("pages/Plugin/".$Plugin."/Sidebar.php"); } ?>
 <div class="modal" id="sh-Modal-Ajax"></div>
 <div class="modal" id="sh-Modal-Waiting"><div class="modal-dialog modal-dialog-centered text-center"><i class="fas fa-cog fa-spin fa-5x mx-auto"></i></div></div>
@@ -29,15 +29,17 @@ if(isset($_GET["DeleteBarndNameID"])){
 		<div class="card border-0 p-0">
 			<?php
 				$result=\P\Brands\lists();
-				while($row = $result->fetch_assoc()) { ?>
-
+				while($row = $result->fetch_assoc()) {
+					$ValueBrandName=$row["Name"];
+					?>
+					
 				<div class="card-body card-hover border-bottom p-2 text-decoration-none text-body" >
 					<div class="" style="float:left;">
 						<?php echo $row["Name"]; ?>
 					</div>
 					<div class="" style="float:right;">
-						<a href="<?php echo $AdminFolder; ?>/Plugin/<?php echo $Plugin; ?>/Pages?EditBarndNameID=<?php echo $row["Id"]; ?>" class="ps-2 pe-1"><i class="fad fa-edit"></i></a>
-						<a href="<?php echo $AdminFolder; ?>/Plugin/<?php echo $Plugin; ?>/dashboard?DeleteBarndNameID=<?php echo $row["Id"]; ?>" class="ps-1 pe-2"><i class="fad fa-trash-alt"></i></a>
+						<a href="<?php echo $AdminFolder; ?>/Plugin/<?php echo $Plugin; ?>/Pages/Add?EditBarndNameID=<?php echo $row["Id"]; ?>" class="ps-2 pe-1"><i class="fad fa-edit"></i></a>
+						<a href="<?php echo $AdminFolder; ?>/Plugin/<?php echo $Plugin; ?>/dashboard?DeleteBarndNameID=<?php echo $row["Id"]; ?>" onclick="return confirm('Brand name [ <?php echo $ValueBrandName; ?> ] wil delete now!')" class="ps-1 pe-2"><i class="fad fa-trash-alt"></i></a>
 					</div>
 				</div>
 			<?php } ?>			
